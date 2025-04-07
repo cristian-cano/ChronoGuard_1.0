3429440299e74639785742bbd995ff7fa769f7fd
Juan Esteban Londoño <juanestebanlc28@gmail.com>
2025-04-07 17:46:46 -0500

    Consolidacion de archivos

---
+++ config.py
@@ -0,0 +1,7 @@
+class config:
+    SECRET_KEY="clave"
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = '8524'
+    MYSQL_DB = 'ChronoDB_db'
+    SESSION_COOKIE_SECURE: False
